1d521f91296cd5a568f047e3b4a9513775d1ba8a
Avi Aryan <avi.aryan123@gmail.com>
2017-05-30 17:23:32 +0530

    open browser and get token from user at login #4

--- appbase/login/login.go
+++ appbase/login/login.go
@@ -1,7 +1,11 @@
 package login
 
 import (
+	"fmt"
+	"github.com/aviaryan/abc/appbase/common"
 	"github.com/aviaryan/abc/appbase/session"
+	"os/exec"
+	"runtime"
 )
 
 // IsUserAuthenticated checks if user is logged in or not
@@ -11,6 +15,43 @@ func IsUserAuthenticated() bool {
 }
 
 // StartUserLogin starts user login process
-func StartUserLogin() error {
+func StartUserLogin(host string) error {
+	url := fmt.Sprintf("%s/login/%s?next=%s/user/token", common.ACCAPI_URL, host, common.ACCAPI_URL)
+	fmt.Printf("Opening %s in the browser.\n", url)
+	fmt.Println("Once authenticated, copy the token from there and paste it into terminal.")
+	// open in browser
+	err := open(url)
+	if err != nil {
+		return err
+	}
+	// read input
+	fmt.Print("> ")
+	var token string
+	fmt.Scanf("%s", &token)
+	// save to file
+	err = session.SaveUserSession(token)
+	if err != nil {
+		return err
+	}
+	// TODO: re-check ?
 	return nil
+}
+
+// https://stackoverflow.com/a/39324149/2295672
+// open opens the specified URL in the default browser of the user.
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
 }
