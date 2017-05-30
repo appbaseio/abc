c1e9f6b3154e72e81f040cc95e24daf19ad09d16
Avi Aryan <avi.aryan123@gmail.com>
2017-05-30 19:11:58 +0530

    Show current user at `abc login` #4

--- cmd/abc/appbase_login.go
+++ cmd/abc/appbase_login.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/aviaryan/abc/appbase/login"
+	"github.com/aviaryan/abc/appbase/user"
 )
 
 // runLogin runs the login command
@@ -17,15 +18,17 @@ func runLogin(args []string) error {
 	switch len(args) {
 	case 0:
 		if login.IsUserAuthenticated() {
-			fmt.Println("Authenticated user")
+			email, _ := user.GetUserEmail()
+			// FIXME: token can be tampered and so it won't work
+			fmt.Println("Logged in as", email)
 		} else {
-			fmt.Println("UnAuthenticated user")
+			fmt.Println("Not logged in.")
 		}
 	case 1:
 		fmt.Println("Logging in..")
 		return login.StartUserLogin(args[0])
 	default:
-		fmt.Println("Wrong parameters. See help.")
+		fmt.Println("Wrong number of parameters. See help (--help).")
 	}
 	return nil
 }
