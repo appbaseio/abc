5e44263e17257e76ae3a6e80747901c66022bddb
Avi Aryan <avi.aryan123@gmail.com>
2017-07-06 10:04:07 +0530

    remove old test files

--- cmd/abc/testdata/test_pipeline_env.js
+++
@@ -1,4 +0,0 @@
-m = mongodb({uri: "${TEST_MONGO_URI}"})
-t.Source("source", m)
-  .Transform("trans", transformer({filename: "pipeline.js"}))
-  .Save("sink", elasticsearch({uri:"http://localhost:9200"}))
