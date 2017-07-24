cdffe3ea9eadddab5fb12c5e72b19a5e03aaef52
Avi Aryan <avi.aryan123@gmail.com>
2017-07-24 23:30:56 +0530

    add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# https://golang.org/doc/install/source#environment
+
+VERSION=0.1.0
+
+export GOARCH=amd64
+
+export GOOS=darwin
+
+go build -o "build/abc-${GOOS}-${VERSION}" -tags '!oss' ./cmd/abc/...
+
+export GOOS=windows
+
+go build -o "build/abc-${GOOS}-${VERSION}.exe"  -tags '!oss' ./cmd/abc/...
+
+export GOOS=linux
+
+go build -o "build/abc-${GOOS}-${VERSION}" -tags '!oss' ./cmd/abc/...
