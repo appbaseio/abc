afd8a3bb239c18779f47dc8fbe676e7b58676ab9
Avi Aryan <avi.aryan123@gmail.com>
2017-08-05 16:39:17 +0530

    base64 encode app json in dejavu open #66

--- appbase/common/dejavu.go
+++ appbase/common/dejavu.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	b64 "encoding/base64"
 	"fmt"
 	"github.com/appbaseio/abc/log"
 	"strings"
@@ -12,7 +13,12 @@ func MakeDejavuURL(appURL string) (string, error) {
 	hostURL := appURL[:idx]
 	appName := appURL[idx+1:]
 	jsonStr := fmt.Sprintf("{\"appname\":\"%s\",\"url\":\"%s\",\"selectedType\":[]}", appName, hostURL)
+	jsonStr = b64.StdEncoding.EncodeToString([]byte(jsonStr))
 	url := "https://opensource.appbase.io/dejavu/live/#?app=" + jsonStr
 	log.Debugln(url)
+	// base64 encode
+	// https://base64-redirect.glitch.me/
+	// encURI := "https://base64-redirect.glitch.me/redirect?to=" + b64.StdEncoding.EncodeToString([]byte(url))
+
 	return url, nil
 }
